66ddcfa99590e4cbe8a9d0255816719ca7d74a55
foysalahmedmin <foysalahmedmin@gmail.com>
2025-11-04 16:37:36 +0600

    Update ENV config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,6 @@
 export const ENV = {
   api_url:
-    (import.meta.env.VITE_API_URL as string) ||
-    "https://admin.mehdihasanrafi.com",
+    (import.meta.env.VITE_API_URL as string) || "https://mehdihasanrafi.com",
   app_url:
     (import.meta.env.VITE_APP_URL as string) || "https://mehdihasanrafi.com",
   environment: import.meta.env.MODE as "development" | "production",
